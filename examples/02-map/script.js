2a39fa5cd630fbd7ccef8572e2b9008928f19b3d
Adam Caron <adamwcaron@gmail.com>
2015-10-13 11:36:26 -0600

    Map over photographs

--- examples/02-map/script.js
+++ examples/02-map/script.js
@@ -1,7 +1,19 @@
-// TODO: Create an array called `simplifiedPhotographs` that takes `photographs` and transforms each one into an object with only two properties: `caption` and `url`.
+// TODO:
+// Create an array called `simplifiedPhotographs`
+// that takes `photographs` and transforms each one into an object with only two properties: `caption` and `url`.
 
-var simplifiedPhotographs = photographs;
+var simplifiedPhotographs = photographs.map(function (photo) {
+  return { caption: photo.caption.text, url: photo.images.low_resolution.url };
+});
 
-// TODO: Create an array called `photographElements` that takes `simplifiedPhotographs` and transforms each one into a DOM element.
+// TODO:
+// Create an array called `photographElements` that takes `simplifiedPhotographs`
+// and transforms each one into a DOM element.
 
-var photographElements = simplifiedPhotographs;
+var photographElements = simplifiedPhotographs.map(function (photo) {
+  var image = document.createElement('img');
+  image.alt = photo.caption;
+  image.src = photo.url;
+  image.className = "instagram-image";
+  return image;
+});
